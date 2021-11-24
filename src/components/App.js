5aa95ba399cf18c1779ff716593add75304ad3a2
hectordeadpoolito <hectordeadpoolito@outlook.es>
2021-11-24 15:10:50 -0700

    React-para-bethany

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-export default class App extends Component {
+import{ BrowserRouter as Router,Switch,Route } from "react-router-dom";
+
+
+import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home"; 
+import About from "./pages/about"; 
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+import "./App.css";
+
+
+  export default class App extends Component {
   render(){
     return(
       <div className='app'>
-        <h1>hector herrera</h1>
+        <Router>
         <div>
-        {moment().format('MMMM Do YYYY, h:mm:ss a')}
+        <h1>Hectordeadpoolito</h1>
+     <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+        <NavigationContainer />
+
+          <Switch>
+            <Route exact Path="/" component={Home}/>
+            <Route Path="/about-me" component={About}/>
+            <Route Path="/contact" component={Contact}/>
+            <Route Path="/blog" component={Blog}/>
+          </Switch>
         </div>
-      </div>
+        </Router>
+        
+    
+        </div>
     );
   }
 }
