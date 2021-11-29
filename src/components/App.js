1199ea0cb7fc26aa5b922262230533584b85cf1a
hectordeadpoolito <hectordeadpoolito@outlook.es>
2021-11-29 12:58:27 -0700

    local host

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,55 @@
-import React, { Component } from 'react';
-import moment from 'moment';
-import{ BrowserRouter as Router,Switch,Route } from "react-router-dom";
+import React, { Component } from "react";
+import moment from "moment";
+import axios from "axios";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-
-import PortfolioContainer from "./portfolio/portfolio-container";
 import NavigationContainer from "./navigation/navigation-container";
-import Home from "./pages/home"; 
-import About from "./pages/about"; 
+import Home from "./pages/home";
+import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
-import "./App.css";
+import PortfolioDetail from "./portfolio/portfolio-detail";
+import NoMatch from "./pages/no-match";
 
 
-  export default class App extends Component {
-  render(){
-    return(
-      <div className='app'>
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.getPortfolioItems = this.getPortfolioItems.bind(this);
+  }
+
+  getPortfolioItems(){
+    axios
+    .get("https://hectordeadpoolito.devcamp.space/portfolio/portfolio_items")
+    .then(response => {
+      console.log(response);
+    })
+  .catch(error => {
+    console.log(error);
+  });
+ }
+  render() {
+    this.getPortfolioItems();
+    return (
+      <div className="app">
         <Router>
-        <div>
-        <h1>Hectordeadpoolito</h1>
-     <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
-        <NavigationContainer />
+          <div>
+            <h1>Jordan Hudgens Portfolio</h1>
+            <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+            <NavigationContainer />
 
-          <Switch>
-            <Route exact Path="/" component={Home}/>
-            <Route Path="/about-me" component={About}/>
-            <Route Path="/contact" component={Contact}/>
-            <Route Path="/blog" component={Blog}/>
-          </Switch>
-        </div>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+              <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
         </Router>
-        
-    
-        </div>
+      </div>
     );
   }
 }
---
+++ src/components/pages/no-match.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Link} from 'react';
+
+export default function(){
+    return (
+        <div>
+        <h2> We couldn't find that pages </h2>
+        <Link to ="/">Return to homepage</Link>
+        </div>
+    );
+}
